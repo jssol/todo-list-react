0d64656b01fb5cae377e9cc0c892e8b0eb7b8ef0
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-22 03:09:41 +0200

    Import the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import TodoContainer from './components/TodoContainer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
