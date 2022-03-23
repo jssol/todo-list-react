7b4d08b637579ddc1289150f9801a512dd9201e7
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-24 00:45:25 +0200

    Refactor InputTodo.js to a function

--- src/components/functionBased/InputTodo.js
+++ src/components/functionBased/InputTodo.js
@@ -1,59 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class InputTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-    };
-  }
+const InputTodo = (props) => {
+  const [title, setTitle] = useState('');
 
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const onChange = (e) => {
+    setTitle(e.target.value);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { title } = this.state;
-    const { addTodoProps } = this.props;
+    const { addTodoProps } = props;
     if (title.trim()) {
       addTodoProps(title);
-      this.setState({
-        title: '',
-      });
+      setTitle('');
     } else {
       alert('Please write item');
     }
   };
 
-  render() {
-    const { title } = this.state;
-    return (
-      <form
-        onSubmit={this.handleSubmit}
-        className="flex justify-between my-6 w-full"
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-between my-6 w-full"
+    >
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+        className="w-full rounded-full bg-slate-100 py-2 px-4 h-10 shadow-md transition-all outline-slate-300"
+      />
+      <button
+        type="submit"
+        className="ml-3 text-sm text-center font-semibold rounded-full shadow-md text-slate-100 bg-green-600 p-2 h-10"
       >
-        <input
-          type="text"
-          placeholder="Add Todo..."
-          value={title}
-          name="title"
-          onChange={this.onChange}
-          className="w-full rounded-full bg-slate-100 py-2 px-4 h-10 shadow-md transition-all outline-slate-300"
-        />
-        <button
-          type="submit"
-          className="ml-3 text-sm text-center font-semibold rounded-full shadow-md text-slate-100 bg-green-600 p-2 h-10"
-        >
-          Submit
-        </button>
-      </form>
-    );
-  }
-}
+        Submit
+      </button>
+    </form>
+  );
+};
 
 InputTodo.propTypes = {
   addTodoProps: PropTypes.func.isRequired,
