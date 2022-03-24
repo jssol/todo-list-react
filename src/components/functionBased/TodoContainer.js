d0f71cf3b7c02439d92d06587eb8239a935738a9
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-24 15:08:35 +0200

    Remove unused imports

--- src/components/functionBased/TodoContainer.js
+++ src/components/functionBased/TodoContainer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Route, Switch } from 'react-router-dom';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
