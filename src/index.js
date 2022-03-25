07a554a61b93c13d19777c84f2d60ef177d29c22
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-25 13:09:29 +0200

    Fix deployment config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import TodoContainer from './components/functionBased/TodoContainer';
 import reportWebVitals from './reportWebVitals';
